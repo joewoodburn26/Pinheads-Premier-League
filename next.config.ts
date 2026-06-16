61da488b050718770e76663fcc9b0f6eb5190df0
joewoodburn26 <joewoodburn26@gmail.com>
2026-06-16 16:13:11 -0400

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   images: {
+    unoptimized: true,
     remotePatterns: [
       { protocol: "https", hostname: "raw.githubusercontent.com" },
       { protocol: "https", hostname: "play.pokemonshowdown.com" },
